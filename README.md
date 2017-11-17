d057aafb930ec26f37b5d06d95c7c97e77493cc7
Tom Pažourek <tom.pazourek@gmail.com>
2017-11-17 23:19:31 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,38 @@
+NaturalSort.Extension
+=====================
+
+*Extension method for `StringComparer` that adds support for natural sorting  (e.g. "abc1", "abc2", "abc10" instead of "abc1", "abc10", "abc2").*
+
+The library is written in C# and released with an [MIT license](https://raw.githubusercontent.com/tompazourek/NaturalSort.Extension/master/LICENSE), so feel **free to fork** or **use commercially**.
+
+**Any feedback is appreciated, please visit the [issues](https://github.com/tompazourek/NaturalSort.Extension/issues?state=open) page or send me an [e-mail](mailto:tom.pazourek@gmail.com).**
+
+Download
+--------
+
+Binaries of the last build can be downloaded on the [AppVeyor CI page of the project](https://ci.appveyor.com/project/tompazourek/NaturalSort.Extension/build/artifacts).
+
+The library is also [published on NuGet.org](https://www.nuget.org/packages/NaturalSort.Extension/) (prerelease), install using:
+
+```
+PM> Install-Package NaturalSort.Extension
+```
+
+<sup>NaturalSort.Extension is built for .NET v4.0, .NET v4.7, .NET Standard 1.3 and .NET Standard 2.0.</sup>
+
+Usage
+-----
+
+Adds `.WithNaturalSort()` extension method to any `StringComparer` you wish to use.
+
+You can use the enhanced comparer in all the places where you can use `IComparer<string>`, e.g. `.OrderBy()` or `.Sort()`.
+
+Sample:
+
+```csharp
+var sequence = new[] { "img12.png", "img10.png", "img2.png", "img1.png" };
+var ordered = sequence.OrderBy(x => x, StringComparer.OrdinalIgnoreCase.WithNaturalSort());
+// ordered will be "img1.png", "img2.png", "img10.png", "img12.png"
+```
+
+For more information about natural sort order, see: [Sorting for Humans: Natural Sort Order (Coding Horror)](https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/).
---
+++ src/NaturalSort.Extension/StringComparerNaturalSortExtension.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NaturalSort.Extension
+{
+    /// <summary>
+    /// Extension for <see cref="StringComparer"/> that adds support for natural sorting.
+    /// </summary>
+    public static class StringComparerNaturalSortExtension
+    {
+        /// <summary>
+        /// Enhances string comparer with natural sorting functionality,
+        /// which allows it to sort numbers inside the strings as numbers, not as letters.
+        /// (e.g. "1", "2", "10" instead of "1", "10", "2")
+        /// </summary>
+        /// <param name="stringComparer">Used string comparer</param>
+        /// <returns>Returns comparer of strings that considers natural sorting.</returns>
+        public static IComparer<string> WithNaturalSort(this StringComparer stringComparer) => new NaturalSortComparer(stringComparer);
+
+        private class NaturalSortComparer : IComparer<string>
+        {
+            public NaturalSortComparer(StringComparer stringComparer)
+            {
+                _stringComparer = stringComparer;
+            }
+
+            /// <summary>
+            /// String comparer used for comparing strings.
+            /// </summary>
+            private readonly StringComparer _stringComparer;
+
+            /// <summary>
+            /// Regex to find sequences of numbers to split.
+            /// </summary>
+            private static readonly Regex NumberSequenceRegex = new Regex(@"(\d+)", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+            public int Compare(string s1, string s2)
+            {
+                var tokens1 = Tokenize(s1);
+                var tokens2 = Tokenize(s2);
+
+                var zipCompare = tokens1.Zip(tokens2, TokenCompare).FirstOrDefault(x => x != 0);
+                if (zipCompare != 0)
+                    return zipCompare;
+
+                var lengthCompare = tokens1.Length.CompareTo(tokens2.Length);
+                return lengthCompare;
+            }
+            
+            /// <summary>
+            /// Splits inputs into tokens, where each token is either a number or a piece of string.
+            /// </summary>
+            private static string[] Tokenize(string s) => s == null ? new string[] { } : NumberSequenceRegex.Split(s);
+
+            /// <summary>
+            /// Parses string as a number, or returns 0 otherwise.
+            /// </summary>
+            private static ulong ParseNumberOrZero(string s) => ulong.TryParse(s, NumberStyles.None, CultureInfo.InvariantCulture, out var result) ? result : 0;
+
+            /// <summary>
+            /// Compares two tokens.
+            /// </summary>
+            private int TokenCompare(string token1, string token2)
+            {
+                var number1 = ParseNumberOrZero(token1);
+                var number2 = ParseNumberOrZero(token2);
+
+                var numberCompare = number1.CompareTo(number2);
+                if (numberCompare != 0)
+                    return numberCompare;
+
+                var stringCompare = _stringComparer.Compare(token1, token2);
+                return stringCompare;
+            }
+        }
+    }
+}
