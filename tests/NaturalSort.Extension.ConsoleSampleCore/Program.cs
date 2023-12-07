f1c5df5c97cda8dc57e54b1f062d24714dba2e63
Tom Pažourek <tom.pazourek@gmail.com>
2023-12-07 10:26:22 +0100

    Re-add files

---
+++ tests/NaturalSort.Extension.ConsoleSampleCore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace NaturalSort.Extension.ConsoleSampleCore;
+
+internal class Program
+{
+    public static void Main()
+    {
+        // Code and data is based on issue: https://github.com/tompazourek/NaturalSort.Extension/issues/2
+        var items = File.ReadAllLines("sample-data.txt").ToArray();
+
+        Console.WriteLine("Item count: " + items.Length);
+
+        var stopwatch = new Stopwatch();
+
+        Console.WriteLine("Sort with StringComparer.OrdinalIgnoreCase");
+
+        stopwatch.Restart();
+        var ordered1 = items.OrderBy(i => i, StringComparer.OrdinalIgnoreCase).ToList();
+        stopwatch.Stop();
+
+        Console.WriteLine("Duration " + stopwatch.Elapsed);
+
+        Console.WriteLine("Sort with StringComparison.OrdinalIgnoreCase.WithNaturalSort()");
+
+        stopwatch.Restart();
+        var ordered2 = items.OrderBy(i => i, StringComparison.OrdinalIgnoreCase.WithNaturalSort()).ToArray();
+        stopwatch.Stop();
+        Console.WriteLine("Duration " + stopwatch.Elapsed);
+
+        Console.ReadKey();
+    }
+}
